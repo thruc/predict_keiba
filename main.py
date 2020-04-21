7793fa2783696074b5645959281ff6101c71acbf
thruc <horiuchi.github@gmail.com>
2020-04-21 15:02:39 +0900

    スクレイピング画像挿入

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ import sys
 from webscrape import scrapedata
 from webscrape import scrapeurl
 
-"""
-ex)python main.py 2020 1 2020 2
-2020年1月から2020年2月までのデータを取得
+def execute(year, mon):
+    urls = scrapeurl.get_urls(year, mon)
+    scrapedata.create_csv(year, mon, urls)
 
-引数1：データを取得したい期間の最初の年
-引数2：データを取得したい期間の最初の月
-引数3：データを取得したい期間の最後の年
-引数4：データを取得したい期間の最後の月
-"""
 
 if __name__ == "__main__":
     args = sys.argv
     print(args)
-    urls = scrapeurl.get_urls(args)
-    scrapedata.create_csv(urls)
+    start_year = int(args[1])
+    end_year = int(args[2])
+    for year in range(start_year, end_year+1):
+        for mon in range(1,13):
+            execute(year, mon)
+            
+
+        
+
+    
